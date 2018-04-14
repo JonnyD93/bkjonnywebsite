3cb75565c145eadf9af7a895c1be5f62165de4ce
Jonny <jond7337@gmail.com>
2018-04-14 11:29:32 -0500

    Tic tac toe Update
    Updated the tic tac toe page
    
    +Cards have been introduced to organize the content.
    +The last ten games are shown
    +Formatting of the page has been updated.

--- src/app/tic-tac-toe/tic-tac-toe.component.ts
+++ src/app/tic-tac-toe/tic-tac-toe.component.ts
@@ -1,55 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 
 @Component({
   selector: 'app-tic-tac-toe',
   templateUrl: './tic-tac-toe.component.html',
   styleUrls: ['./tic-tac-toe.component.css']
 })
-export class TicTacToeComponent {
+export class TicTacToeComponent implements AfterViewInit {
 
   board: number[];
 
-  turn: boolean = false;
+  turn = false;
   lastWinner: string;
   o: any = {health: 5};
+  last10: any = [];
 
-  constructor(){
-    this.resetBoard();
-    this.lastWinner = localStorage.getItem("lastWinner");
-    console.log(JSON.stringify(this.o));
-    console.log(JSON.parse(JSON.stringify(this.o)));
+  ngAfterViewInit() {
+    document.body.style.backgroundColor = '#ffffff';
   }
 
-  resetBoard(){
-    this.board = [0,0,0,0,0,0,0,0,0];
+  constructor() {
+    this.resetBoard();
+    this.last10 = JSON.parse(localStorage.getItem('last10Winners'));
+  }
+
+  resetBoard() {
+    this.board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
     this.turn = false;
   }
 
-setLastWinner(str){
+/*setLastWinner(str) {
   this.lastWinner = str;
-  localStorage.setItem("lastWinner", this.lastWinner);
-}
+  localStorage.setItem('lastWinner', this.lastWinner);
+}*/
+  setLast10(str) {
+    this.last10.unshift(str);
+    if (this.last10.length > 10) {
+      this.last10.splice(10, 1);
+    }
+    localStorage.setItem('last10Winners', JSON.stringify(this.last10));
+  }
 
-  select(index){
-    if(this.board[index] === 0){
+  select(index) {
+    if (this.board[index] === 0) {
       this.board[index] = this.turn ? 2 : 1;
-      if(this.checkWin(this.turn ? 2 : 1)){
-        this.setLastWinner("Player " + (this.turn ? 2 : 1) + " Won!");
-        console.log(this.lastWinner);
+      if (this.checkWin(this.turn ? 2 : 1)) {
+        this.setLast10('Player ' + (this.turn ? 2 : 1) + ' Won!');
         this.resetBoard();
-        return "tits";
+        return 'null';
       }
-      if(this.checkTie()){
-        this.setLastWinner("Tie!");
+      if (this.checkTie()) {
+        this.setLast10('Tie!');
         this.resetBoard();
-        return "tits";
+        return 'null';
       }
       this.turn = !this.turn;
     }
   }
 
-  checkWin(val){
-    if(
+  checkWin(val) {
+    if (
       this.allEqual(val, [0, 1, 2]) ||
       this.allEqual(val, [3, 4, 5]) ||
       this.allEqual(val, [6, 7, 8]) ||
@@ -58,17 +67,18 @@ setLastWinner(str){
       this.allEqual(val, [2, 5, 8]) ||
       this.allEqual(val, [0, 5, 8]) ||
       this.allEqual(val, [2, 5, 6])
-    )
-    return true;
+    ) {
+      return true;
+    }
     return false;
   }
 
-  checkTie(){
-    return this.board.filter(val=>val===0).length === 0
+  checkTie() {
+    return this.board.filter(val => val === 0).length === 0;
   }
 
-  allEqual(val, ar){
-    return ar.filter(a=>this.board[a]===val).length === ar.length;
+  allEqual(val, ar) {
+    return ar.filter(a => this.board[a] === val).length === ar.length;
   }
 
 }
