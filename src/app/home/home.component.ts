0130495981574d8f5bdfbdaf8c7257b0c1fba032
Jonny <jond7337@gmail.com>
2018-05-21 18:55:58 -0400

    Updated Home Page
    +Added the ability to embed any embeddable page.
    +Added the ability to navigate to other 'pages' on the website.
    -Moved the background of the homepage
    +Cleaned up html

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {createElement} from "@angular/core/src/view/element";
+import {appendChild} from "@angular/core/src/render3/node_manipulation";
 
 @Component({
   selector: 'app-home',
@@ -7,31 +9,87 @@ import {AfterViewInit, Component, OnInit} from '@angular/core';
 })
 export class HomeComponent implements OnInit, AfterViewInit {
 
-  blogdata: { title: string, date: string, content: string, image: string }[] = [{
-    title: "Website Update 1",
+  activePosts: { title: string, date: string, content: string, images: string[], embedded: string[]} [] = []; //Active Posts on Page
+  pageIndex: number; //The Page we are On
+  amtPostsShown: number = 5; //The Amount of Posts Shown on Each Page
+  NumOfPages: number[] = []; // The Amount of Pages to choose from
+  Posts: { title: string, date: string, content: string, images: string[], embedded: string[] }[] = [{ //Temp until synchronized with Firebase
+    title: "Website Update 6",
     date: "5/18/2018",
     content: "Started off by creating the home page, which if you are unable to tell, I plan it to be like a blog. I will post weekly updates here, and allow for easy maneuvering to other updates. As of right now, the entire card is based off of the html. I plan to move this into a firebase database, and have my own system in which I can log in, and change my website easily. All of this is being done in Angular 2, and in Typescript.",
-    image: ""
+    images: [],
+    embedded: ["<iframe width='560' height='315' src=\"https://www.youtube.com/embed/H6HsalSk-ZM\" frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>","<iframe width='560' height='315' src=\"https://www.youtube.com/embed/H6HsalSk-ZM\" frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>"]
+  },{
+    title: "Website Update 5",
+    date: "5/18/2018",
+    content: "Started off by creating the home page, which if you are unable to tell, I plan it to be like a blog. I will post weekly updates here, and allow for easy maneuvering to other updates. As of right now, the entire card is based off of the html. I plan to move this into a firebase database, and have my own system in which I can log in, and change my website easily. All of this is being done in Angular 2, and in Typescript.",
+    images: ["../../assets/images/logo/altYinYangYong.png"],
+    embedded: []
+  },{
+    title: "Website Update 4",
+    date: "5/18/2018",
+    content: "Started off by creating the home page, which if you are unable to tell, I plan it to be like a blog. I will post weekly updates here, and allow for easy maneuvering to other updates. As of right now, the entire card is based off of the html. I plan to move this into a firebase database, and have my own system in which I can log in, and change my website easily. All of this is being done in Angular 2, and in Typescript.",
+    images: [],
+    embedded: []
+  },{
+    title: "Website Update 3",
+    date: "5/18/2018",
+    content: "Started off by creating the home page, which if you are unable to tell, I plan it to be like a blog. I will post weekly updates here, and allow for easy maneuvering to other updates. As of right now, the entire card is based off of the html. I plan to move this into a firebase database, and have my own system in which I can log in, and change my website easily. All of this is being done in Angular 2, and in Typescript.",
+    images: [],
+    embedded: []
+  },{
+    title: "Website Update 2",
+    date: "5/18/2018",
+    content: "Started off by creating the home page, which if you are unable to tell, I plan it to be like a blog. I will post weekly updates here, and allow for easy maneuvering to other updates. As of right now, the entire card is based off of the html. I plan to move this into a firebase database, and have my own system in which I can log in, and change my website easily. All of this is being done in Angular 2, and in Typescript.",
+    images: [],
+    embedded: []
   }, {
     title: "Website Update 1",
     date: "5/18/2018",
     content: "Started off by creating the home page, which if you are unable to tell, I plan it to be like a blog. I will post weekly updates here, and allow for easy maneuvering to other updates. As of right now, the entire card is based off of the html. I plan to move this into a firebase database, and have my own system in which I can log in, and change my website easily. All of this is being done in Angular 2, and in Typescript.",
-    image: "../../assets/images/logo/altYinYangYong.png"
+    images: ["../../assets/images/logo/altYinYangYong.png","../../assets/images/logo/altYinYangYong.png"],
+    embedded: ["<iframe width='100%' height='300' scrolling='no' frameborder='no' allow='autoplay' src=\"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/339318387&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true\"></iframe>"]
   }
   ];
 
   constructor() {
+    this.pageIndex = 0; // The page index is the page we are on.
+    this.updatePage();
   }
 
+  checkNumOfPages(){
+    this.NumOfPages = [];
+    for(let i = 0; i < this.Posts.length/this.amtPostsShown ; i++ )
+      this.NumOfPages.push(i);
+  }
 
+  changePage(Right:boolean){
+    if(Right)
+      this.pageIndex++;
+    else {
+      if(this.pageIndex-1 < 0){
+        this.pageIndex = 0;
+      }
+      else
+        this.pageIndex--;
+    }
+      this.updatePage();
+  }
+
+  updatePage(){
+    this.activePosts = [];
+    for(let i = (this.pageIndex*this.amtPostsShown); i < this.Posts.length && i < (this.amtPostsShown + (this.pageIndex*this.amtPostsShown)) ; i++ ) {
+     this.activePosts.push(this.Posts[i]);
+    }
+    this.checkNumOfPages();
+  }
+  updateEmbedded(url:string){
+    return url;
+  }
   ngOnInit() {
   }
 
   ngAfterViewInit() {
-    window['$'](document).ready(function () {
-      window['$']('.parallax').parallax();
-    });
-
   }
 
 }
